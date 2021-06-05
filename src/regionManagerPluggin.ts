f4f42058943a3c133d54a2d65d5a4e0dadeb72f0
Francis Enyi <39003759+geekhybrid@users.noreply.github.com>
2021-06-05 15:41:37 +0100

    Added region-wrapper component to plugin

--- src/regionManagerPluggin.ts
+++ src/regionManagerPluggin.ts
@@ -4,6 +4,7 @@ import { REGION_PREFIX } from "./@types/constants";
 import { RegionManager, RegionInfo } from "./@types/types";
 import { regionNameDirective } from "./directives/regionNameDirective";
 import { regionActivationDirective } from "./directives/regionActivationDirective";
+import RegionWrapper from "./components/region-wrapper.vue";
 
 const regions: {[regionName: string]: RegionInfo[]} = {};
 
@@ -12,7 +13,7 @@ const regionManager: RegionManager = {
         const regionHost = document.getElementsByClassName(`${REGION_PREFIX}${regionName}`);
         if (regionHost.length == 0)
         {
-            console.log(`Unable to find region ${regionName}. ${component.name} was not mounted.`);
+            console.log(`Unable to find region ${regionName}. As a result, ${component.name} was not mounted.`);
             return;
         }
         if (regionHost.length > 1) {
@@ -28,6 +29,7 @@ const regionManager: RegionManager = {
 export const regionManagerPlugin: Plugin = {
     install(app: App): void {
         app.config.globalProperties.$regionManager = regionManager;
+        app.component('v-region', RegionWrapper);
         app.directive("region", regionNameDirective);
         app.directive("activation", regionActivationDirective);
     }
